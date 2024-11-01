8c3719f1c9c0611b26b3b39caf2eb635c29e06aa
Sharif Alalem <sharif.alalem@savings-united.com>
2024-11-02 00:19:08 +0100

    fix project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/markdown-previewer/',
+  base: '/react-calculator/',
   css: {
       preprocessorOptions: {
         scss: {
