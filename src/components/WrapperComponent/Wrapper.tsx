6d4e2259a7661a9281de12fbdbbb7efa71c7cd7e
Sharif Alalem <sharif.alalem@savings-united.com>
2024-11-02 01:02:26 +0100

    code readability improve & fix multi operations case

--- src/components/WrapperComponent/Wrapper.tsx
+++ src/components/WrapperComponent/Wrapper.tsx
@@ -27,37 +27,40 @@ class Wapper extends Component<{},WrapperState> {
         }
     }
 
+    cleanExpression = (expression:string) => {
+        return expression.replace(/([-+*/]){2,}/g, (match:any) => {
+          return match.endsWith('-') ? match.slice(-2) : match.slice(-1);
+        });
+    };
+
     calculateResult = () => {
         const mexp = new Mexp();
-        const evaluatedResult = mexp.eval(this.state.display);
+        const evaluatedResult = mexp.eval(this.cleanExpression(this.state.display));
         flushSync(()=>{this.setState({display: String(evaluatedResult),result: evaluatedResult})});
     };
 
+    resetCalculator = () => {
+        flushSync(()=>{this.setState({
+            result: 0,
+            display: '0',
+            leftZero:true,
+            decimalApplied: false,
+        })});
+    }
+
     handleClick = (e:any)=>{
         let char = String(e.target.innerHTML);
-        if(char =='AC'){
-            flushSync(()=>{this.setState({
-                result: 0,
-                display: '0',
-                leftZero:true
-            })});
-        }
-        else if(char === '='){
-            this.calculateResult();
-        } else {
-            if(this.state.result != 0) {
-                flushSync(()=>{this.setState({display: String(this.state.result)+ char,result:0})});
-            }
+        if(char =='AC') this.resetCalculator();
+        else if(char === '=') this.calculateResult();
+        else {
+            if(this.state.result != 0) flushSync(()=>{this.setState({display: String(this.state.result)+ char,result:0})});
             else {
                 if(operations.has(char)){
                     if(char == '.' && this.state.decimalApplied == false)
                         flushSync(()=>{this.setState({display: this.state.display + char, leftZero:true,decimalApplied:true})})
                     else if(char == '.' && this.state.decimalApplied == true)
                         flushSync(()=>{this.setState({display: this.state.display, leftZero:true,decimalApplied:true})})  
-                    else if(operations.has(this.state.display[this.state.display.length-1]) && char !== '-')
-                        flushSync(()=>{this.setState({display: this.state.display.slice(0,-1) + char,decimalApplied:false, leftZero:true})}) 
-                    else
-                        flushSync(()=>{this.setState({display: this.state.display + char,decimalApplied:false, leftZero:true})}) 
+                    else  flushSync(()=>{this.setState({display: this.state.display + char,decimalApplied:false, leftZero:true})}) 
                 }
                 else {
                     if(this.state.leftZero == true && char !== '0' && this.state.display[this.state.display.length-1] == '0')
